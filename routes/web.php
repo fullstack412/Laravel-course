2bd68042d5604b0db57dca19c5680eb0aed6106f
fullstack412 <dt897867@gmail.com>
2019-09-10 08:38:13 -0700

    route model binding

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('todos', 'TodosController@index');
-Route::get('todos/{todos}', 'TodosController@show');
+Route::get('todos/{todo}', 'TodosController@show');
 Route::get('new-todos', 'TodosController@create');
 
 Route::post('store-todos', 'TodosController@store');
